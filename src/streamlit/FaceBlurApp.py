09f9f398ea7b835c6fa78c93473e6c417fa46e00
unknown <josgonmar14@gmail.com>
2022-08-07 02:50:44 +0200

    Faces are now blurred using an elliptical mask

--- src/streamlit/FaceBlurApp.py
+++ src/streamlit/FaceBlurApp.py
@@ -34,7 +34,7 @@ class FaceBlurApp:
             detection_threshold = streamlit.slider('Detection threshold', 0.5, 1.0, 0.75)
             blur_factor = streamlit.slider('Blur factor', self.__min_blur_factor, self.__max_blur_factor, self.__min_blur_factor)
 
-            input_image, faces = self.__generateFaceBlurRect(image, detections, detection_threshold)
+            input_image, faces = self.__generateFaceBlurBounds(image, detections, detection_threshold)
 
             with input_col:
                 streamlit.subheader('Original input image')
@@ -70,11 +70,11 @@ class FaceBlurApp:
         href = f'<a href="data:file/txt;base64,{img_str}" download="{filename}">Download output</a>'
         return href
 
-    def __generateFaceBlurRect(self, image, detections, threshold):
+    def __generateFaceBlurBounds(self, image, detections, threshold):
         height, width = image.shape[:2]
         input_image = image.copy()
         face_count = 0
-        selected_faces_data = []
+        detected_faces_data = []
 
         for i in range(detections.shape[2]):
             confidence = detections[0, 0, i, 2]
@@ -87,24 +87,32 @@ class FaceBlurApp:
                 cv2.putText(input_image, str(face_count), txt_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, 2)
                 cv2.rectangle(input_image, (x1,y1), (x2,y2), (0,0,255), 1, 1)
 
-                selected_faces_data.append(['Face ' + str(face_count), x1, y1, x2, y2])
+                detected_faces_data.append(['Face ' + str(face_count), x1, y1, x2, y2])
                 face_count += 1
         
-        return input_image, selected_faces_data
+        return input_image, detected_faces_data
     
     def __blurFaces(self, image, faces, factor):
         output_image = image.copy()
-
+        blurred_image = image.copy()
+        ellipse_mask = np.zeros(image.shape, dtype=image.dtype)
         for data in faces:
-            x1 = data[1]
-            y1 = data[2]
-            x2 = data[3]
-            y2 = data[4]
+            x1 = int(data[1])
+            y1 = int(data[2])
+            x2 = int(data[3])
+            y2 = int(data[4])
 
-            face = output_image[y1:y2, x1:x2]
+            face = output_image[y1:y2, x1:x2, :]
             face = self.__blurFace(face, factor)
 
-            output_image[y1:y2, x1:x2] = face
+            blurred_image[y1:y2, x1:x2, :] = face
+
+            e_center = ((x2-x1)/2 + x1, (y2-y1)/2 + y1)
+            e_size = (x2 - x1, y2 - y1)
+            e_angle = 0.0
+
+            ellipse_mask = cv2.ellipse(ellipse_mask, (e_center, e_size, e_angle), (255, 255, 255), -1, cv2.LINE_AA)
+            np.putmask(output_image, ellipse_mask, blurred_image)
 
         return output_image
     
